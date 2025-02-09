64ac2059a4eba4bba94d45329059c9e45d50f27e
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-02-09 20:04:17 +0100

    Make Damage GameplayEffect Spec Handle

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -32,6 +32,8 @@ namespace WarriorGameplayTags
 
 	/** Shared Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_MeleeHit, "Shared.Event.MeleeHit");
+	UE_DEFINE_GAMEPLAY_TAG(Shared_SetByCaller_BaseDamage, "Shared.SetByCaller.BaseDamage");
+	
 	
 
 }
