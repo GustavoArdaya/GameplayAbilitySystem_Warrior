03e4f47cc5661cffb44268bc332cd3d0f29b1b67
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-02-18 17:10:39 +0100

    Add Enemy AI an perception

---
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -0,0 +1,34 @@
+// Gustavo Ardaya
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "WarriorAIController.generated.h"
+
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+struct FAIStimulus;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+};
