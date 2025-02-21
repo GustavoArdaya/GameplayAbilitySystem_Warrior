03407517c9f9decfa1c75049d45fa227674ce16d
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-02-21 16:40:22 +0100

    Add melee hit notify

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,12 +2,39 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
 #include "WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor)) return;
+
+	OverlappedActors.AddUnique(HitActor);
+
+	bool bIsValidBlock = false;
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
