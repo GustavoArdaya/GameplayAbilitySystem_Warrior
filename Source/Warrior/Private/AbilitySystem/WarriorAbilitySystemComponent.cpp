dbd3d9a9d40740aeaacc1a9baa04d44426ff060b
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-02-21 12:03:16 +0100

    add Activate ability by tag to AbilitySystemComponent. Add Dummy test enemy

--- Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -2,8 +2,7 @@
 
 
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
-
-#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
 #include "WarriorDebugHelper.h"
 
 void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& InInputTag)
@@ -54,4 +53,26 @@ void UWarriorAbilitySystemComponent::RemoveGrantedHeroWeaponAbilities(UPARAM(ref
 		}
 	}
 	InSpecHandlesToRemove.Empty();
+}
+
+bool UWarriorAbilitySystemComponent::TryActivateAbilityByTag(FGameplayTag AbilityTagToActivate)
+{
+	check(AbilityTagToActivate.IsValid());
+
+	TArray<FGameplayAbilitySpec*> FoundAbilitySpecs;
+	GetActivatableGameplayAbilitySpecsByAllMatchingTags(AbilityTagToActivate.GetSingleTagContainer(), FoundAbilitySpecs);
+
+	if (!FoundAbilitySpecs.IsEmpty())
+	{
+		const int32 RandomAbilityIndex = FMath::RandRange(0, FoundAbilitySpecs.Num() - 1);
+		FGameplayAbilitySpec* SpecToActivate = FoundAbilitySpecs[RandomAbilityIndex];
+
+		check(SpecToActivate);
+
+		if (!SpecToActivate->IsActive())
+		{
+			return TryActivateAbility(SpecToActivate->Handle);
+		}
+	}
+	return false;
 }
