38a65eea69ad96b3209938cd9c743843cbb1b334
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-02-24 18:09:20 +0100

    determine roll distance by level

--- Source/Warrior/Public/WarriorFunctionLibrary.h
+++ Source/Warrior/Public/WarriorFunctionLibrary.h
@@ -9,7 +9,7 @@
 
 class UWarriorAbilitySystemComponent;
 class UPawnCombatComponent;
-
+struct FScalableFloat;
 
 
 /**
@@ -42,4 +42,8 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "Warrior|FunctionLibrary")
 	static bool IsTargetPawnHostile(APawn* QueryPawn, APawn* TargetPawn);
+
+	UFUNCTION(BlueprintPure, Category = "Warrior|FunctionLibrary", meta = (CompactNodeTitle = "Get Value At Level"))
+	static float GetScalableFloatValueAtLevel(const FScalableFloat& InScalableFloat, float InLevel = 1.f);
+
 };
