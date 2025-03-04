b81ccd7df563f62c51fd8d8363f1f1ad20ab46f4
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-03-04 10:38:03 +0100

    Handle Switch Target

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -142,7 +142,7 @@ void AWarriorHeroCharacter::Input_SwitchTargetCompleted(const FInputActionValue&
 		Data
 	);
 
-	Debug::Print(TEXT("SwitchDirection: " + SwitchDirection.ToString()));
+	
 }
 
 void AWarriorHeroCharacter::Input_AbilityInputPressed(FGameplayTag InInputTag)
