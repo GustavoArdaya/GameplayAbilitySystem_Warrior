073a28391d04c57199e0c810965e209768f34a2a
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-04-08 19:15:28 +0200

    Add Collect Stones Function to Pickup Stones GA

--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h
@@ -6,6 +6,9 @@
 #include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
 #include "HeroGameplayAbility_PickUpStones.generated.h"
 
+
+class AWarriorStoneBase;
+
 /**
  * 
  */
@@ -21,4 +24,23 @@ protected:
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	// ~ End UGameplayAbility Interface
 	
+	UFUNCTION(BlueprintCallable)
+	void CollectStones();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	float BoxTraceDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector TraceBoxSize = FVector(100.f);
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TEnumAsByte<EObjectTypeQuery>> StoneTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool bDrawDebugShape = false;
+
+	UPROPERTY()
+	TArray<AWarriorStoneBase*> CollectedStones;
 };
