b78c239277c87b65b9a26438dc2b29468530aefd
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-04-22 08:51:46 +0200

    Add WarriorGameInstance and getter function in library

--- Source/Warrior/Private/WarriorFunctionLibrary.cpp
+++ Source/Warrior/Private/WarriorFunctionLibrary.cpp
@@ -9,6 +9,7 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "WarriorGameplayTags.h"
 #include "WarriorTypes/WarriorCountDownAction.h"
+#include "WarriorGameInstance.h"
 #include "WarriorDebugHelper.h"
 
 UWarriorAbilitySystemComponent* UWarriorFunctionLibrary::NativeGetWarriorASCFromActor(AActor* InActor)
@@ -177,4 +178,16 @@ void UWarriorFunctionLibrary::CountDown(const UObject* WorldContextObject, float
 			FoundAction->CancelAction();
 		}
 	}
+}
+
+UWarriorGameInstance* UWarriorFunctionLibrary::GetWarriorGameInstance(const UObject* WorldContextObject)
+{
+	if (GEngine)
+	{
+		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			return World->GetGameInstance<UWarriorGameInstance>();
+		}
+	}
+	return nullptr;
 }
