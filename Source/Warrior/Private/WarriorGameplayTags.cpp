1b88df84f06942ee168d717d1aa6c1b26a13f3be
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-04-22 09:28:10 +0200

    Add GameInstance

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -99,6 +99,10 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Status_HitReact_Back, "Shared.Status.HitReact.Back");
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Status_Invincible, "Shared.Status.Invincible");
 
+
+	UE_DEFINE_GAMEPLAY_TAG(GameData_Level_SurvivalGameModeMap, "GameData.Level.SurvivalGameModeMap");
+	UE_DEFINE_GAMEPLAY_TAG(GameData_Level_MainMenuMap, "GameData.Level.MainMenuMap");
+
 	
 	
 
--- Source/Warrior/Public/WarriorGameInstance.h
+++ Source/Warrior/Public/WarriorGameInstance.h
@@ -4,7 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "GameplayTagContainer.h"
 #include "WarriorGameInstance.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWarriorGameLevelSet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, meta = (Categories = "GameData.Level"))
+	FGameplayTag LevelTag;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> Level;
+
+	bool IsValid() const
+	{
+		return LevelTag.IsValid() && !Level.IsNull();
+	}
+};
+
 
 /**
  * 
@@ -13,5 +32,15 @@ UCLASS()
 class WARRIOR_API UWarriorGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FWarriorGameLevelSet> GameLevelSets;
+
+public:
+
+	UFUNCTION(BlueprintPure, meta = (GameplayTagFilter = "GameData.Level"))
+	TSoftObjectPtr<UWorld> GetGameLevelByTag(FGameplayTag InTag) const;
 	
 };
