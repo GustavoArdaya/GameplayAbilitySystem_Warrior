eb8d744abffeea83870c9ce24080a367d521782e
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-04-22 09:36:23 +0200

    Toggle Input Mode on try again

--- Source/Warrior/Private/WarriorFunctionLibrary.cpp
+++ Source/Warrior/Private/WarriorFunctionLibrary.cpp
@@ -191,3 +191,38 @@ UWarriorGameInstance* UWarriorFunctionLibrary::GetWarriorGameInstance(const UObj
 	}
 	return nullptr;
 }
+
+void UWarriorFunctionLibrary::ToggleInputMode(const UObject* WorldContextObject, EWarriorInputMode InInputMode)
+{
+	APlayerController* PlayerController = nullptr;
+
+	if (GEngine)
+	{
+		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			PlayerController = World->GetFirstPlayerController();
+		}
+	}
+
+	if (!PlayerController) return;
+
+	FInputModeGameOnly GameOnlyMode;
+	FInputModeGameOnly UIOnlyMode;
+
+	switch (InInputMode)
+	{
+	case EWarriorInputMode::GameOnly:
+		PlayerController->SetInputMode(GameOnlyMode);
+		PlayerController->bShowMouseCursor = false;
+		break;
+
+	case EWarriorInputMode::UIOnly:
+		PlayerController->SetInputMode(UIOnlyMode);
+		PlayerController->bShowMouseCursor = true;
+		break;
+
+	default:
+		break;
+
+	}
+}
